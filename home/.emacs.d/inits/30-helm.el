a349bd99ba9cbcdf516356d62c830a3cadf76569
mickato <mickato@gmail.com>
2015-08-20 10:58:05 +0900

    fix

--- home/.emacs.d/inits/30-helm.el
+++ home/.emacs.d/inits/30-helm.el
@@ -1,4 +1,8 @@
 (require 'helm-config)
 (require 'helm-ag)
+(require 'helm-descbinds)
 (helm-mode 1)
+(helm-descbinds-mode)
+(custom-set-variables
+ '(helm-ag-base-command "pt -e --nocolor --nogroup"))
 (global-set-key "\C-l" 'helm-mini)
--- home/.emacs.d/inits/40-web.el
+++ home/.emacs.d/inits/40-web.el
@@ -1,7 +1,7 @@
 (autoload 'web-mode "web-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-hook 'web-mode-hook
-          '(lamvda ()
+          '(lambda ()
                    (setq web-mode-markup-indent-offset 2)
                    (setq web-mode-css-indent-offset 2)
                    (setq web-mode-code-indent-offset 2)
